f91e4e2fcd6a498b86844ac18f049b8f029b1fbc
kvasa <kvasan2104@gmail.com>
2023-06-27 20:16:45 +0300

    working in show_books_menu

--- include/menus.h
+++ include/menus.h
@@ -12,10 +12,18 @@ enum admin_choice {
 };
 
 enum books_choice {
-
+    EXIT = 0,
+    ADD_A_BOOK,
+    DELETE_A_BOOK,
+    INFO_ABOUT_A_BOOK,
+    ALL_BOOK_INFO,
+    EDIT_BOOK_INFO,
+    CHANGE_BOOK_AMOUNT,
+    GIVE_A_BOOK,
+    RECEIVE_A_BOOK
 };
 
-void show_books_menu();
+void show_books_menu(bool link_to_admin_menu);
 
 void show_students_menu();
 
--- src/menus.c
+++ src/menus.c
@@ -12,7 +12,7 @@ void show_admin_menu() {
     } while (choice != 1 && choice != 2);
     switch (choice) {
         case SHOW_BOOKS_MENU:
-            show_books_menu();
+            show_books_menu(true);
             break;
         case SHOW_STUDENTS_MENU:
             show_students_menu();
@@ -22,8 +22,31 @@ void show_admin_menu() {
     }
 }
 
-void show_books_menu() {
-
+void show_books_menu(bool link_to_admin_menu) {
+    puts("1. Add a new book\n2. Delete a book\n3. See information about a book\n4. See information about"
+         " all books\n5. Edit book info\n6. Change amount of available books\n7. Give a book to the student\n"
+         "8. Receive a book from a student\n");
+    if (link_to_admin_menu)
+        puts("0. Exit to admin menu\n");
+    char input;
+    int choice;
+    int entry_count = 0;
+    do {
+        if (entry_count > 0)
+            puts("Please enter a specified number as a choice");
+        scanf("%c", &input);
+        entry_count++;
+    } while (!isdigit(input) && atoi(&input) == 9);
+    choice = atoi(&input);
+    switch (choice) {
+        case ADD_A_BOOK:
+            break;
+        case DELETE_A_BOOK:
+            break;
+        case INFO_ABOUT_A_BOOK:
+            break;
+        case
+    }
 }
 
 void show_student_menu() {
