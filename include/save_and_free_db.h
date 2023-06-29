e8ee921fd77b9d75b46fab10da67fda4f3f27405
kvasa <kvasan2104@gmail.com>
2023-06-29 12:57:33 +0300

    save and free done

--- include/save_and_free_db.h
+++ include/save_and_free_db.h
@@ -1,8 +1,22 @@
-//
-// Created by kvasa on 28.06.2023.
-//
-
 #ifndef BIG_HW_SAVE_AND_FREE_DB_H
 #define BIG_HW_SAVE_AND_FREE_DB_H
 
-#endif //BIG_HW_SAVE_AND_FREE_DB_H
+#include <stdio.h>
+#include <stdlib.h>
+#include "init_db.h"
+
+void save_books(Book_t *book_db_ptr, int number_of_books);
+
+void save_students(Student_t *stud_db_ptr, int number_of_students);
+
+void save_student_books(StudentBook_t *stud_book_db_ptr, int number_of_student_books);
+
+void free_books_db(Book_t *book_db_ptr, int number_of_books);
+
+void free_students_db(Student_t *stud_db_ptr, int number_of_students);
+
+void free_users_db(User_t *user_db_ptr, int number_of_users);
+
+void free_student_book_db(StudentBook_t *stud_book_db_ptr, int number_of_student_books);
+
+#endif
--- src/save_and_free_db.c
+++ src/save_and_free_db.c
@@ -1,5 +1,69 @@
-//
-// Created by kvasa on 28.06.2023.
-//
-
 #include "../include/save_and_free_db.h"
+
+void save_books(Book_t *book_db_ptr, int number_of_books) {
+    FILE *file_ptr = fopen(BOOKS_DB_FILE_PATH, "w");
+    for (int i = 0; i < number_of_books; i++) {
+        fprintf(file_ptr, "%ld,%s,%s,%d,%d", book_db_ptr[i].ISBN, book_db_ptr[i].author,
+                book_db_ptr[i].title, book_db_ptr[i].total_books, book_db_ptr[i].available_books);
+        if (i != number_of_books - 1)
+            fputs("\n", file_ptr);
+    }
+    fclose(file_ptr);
+}
+
+void save_students(Student_t *stud_db_ptr, int number_of_students) {
+    FILE *file_ptr = fopen(STUDENTS_DB_FILE_PATH, "w");
+    for (int i = 0; i < number_of_students; i++) {
+        fprintf(file_ptr, "%s,%s,%s,%s,%s", stud_db_ptr[i].record_book_num,
+                stud_db_ptr[i].surname, stud_db_ptr[i].name, stud_db_ptr[i].faculty, stud_db_ptr[i].speciality);
+        if (i != number_of_students - 1)
+            fputs("\n", file_ptr);
+    }
+    fclose(file_ptr);
+}
+
+void save_student_books(StudentBook_t *stud_book_db_ptr, int number_of_student_books) {
+    FILE *file_ptr = fopen(STUDENTS_BOOKS_DB_FILE_PATH, "w");
+    for (int i = 0; i < number_of_student_books; i++) {
+        fprintf(file_ptr, "%ld,%s,%s", stud_book_db_ptr[i].ISBN, stud_book_db_ptr[i].record_book_num,
+                stud_book_db_ptr[i].return_date);
+        if (i != number_of_student_books - 1)
+            fputs("\n", file_ptr);
+    }
+    fclose(file_ptr);
+}
+
+void free_books_db(Book_t *book_db_ptr, int number_of_books) {
+    for (int i = 0; i < number_of_books; i++) {
+        free(book_db_ptr[i].title);
+        free(book_db_ptr[i].author);
+    }
+    free(book_db_ptr);
+}
+
+void free_students_db(Student_t *stud_db_ptr, int number_of_students) {
+    for (int i = 0; i < number_of_students; i++) {
+        free(stud_db_ptr[i].record_book_num);
+        free(stud_db_ptr[i].surname);
+        free(stud_db_ptr[i].name);
+        free(stud_db_ptr[i].faculty);
+        free(stud_db_ptr[i].speciality);
+    }
+    free(stud_db_ptr);
+}
+
+void free_users_db(User_t *user_db_ptr, int number_of_users) {
+    for (int i = 0; i < number_of_users; i++) {
+        free(user_db_ptr[i].login);
+        free(user_db_ptr[i].hash);
+    }
+    free(user_db_ptr);
+}
+
+void free_student_book_db(StudentBook_t *stud_book_db_ptr, int number_of_student_books) {
+    for (int i = 0; i < number_of_student_books; i++) {
+        free(stud_book_db_ptr[i].record_book_num);
+        free(stud_book_db_ptr[i].return_date);
+    }
+    free(stud_book_db_ptr);
+}
